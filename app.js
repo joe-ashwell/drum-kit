ee6261525449ecc15057db01bcc566f4c60ff77b
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-08-18 14:21:54 +0200

    Update to work on click too

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-
+// Function to play the correct sound to the corresponding key
 function playSound(e) {
+
   // Creates the audio variable by using the query selector and the CSS attribute selector to grab the data key (using template literals to dynamically match the event's keycode with that of the audio keycode)
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-  // If there's no audio, it stops the function running.
+  // If there's no audio, it stops the function running
   if ( !audio ) {
     return;
   }
@@ -12,6 +13,8 @@ function playSound(e) {
   // This rewinds the audio file to the beginning as if you call an audio element to play whilst it's already playing, it won't do anything
   audio.currentTime = 0;
   audio.play();
+
+  // This adds the class of .playing to key when it's played
   key.classList.add('playing');
 
 }
@@ -27,8 +30,32 @@ function removeTransition(e) {
 
 }
 
+// Creates the variable of keys for each div with the .key class
 const keys = document.querySelectorAll('.key');
+
+// Uses a forEach loop to go through to target each key
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-// Listens to the entire page for a keydown, it then captures the event
+// Listens to the entire page for a keydown, it then captures the event and runs the playSound function
 window.addEventListener('keydown', playSound);
+
+// Uses a forEach loop to go through to target each key when clicked -> was annoying due to the key reference, so had to change to 'item'.
+
+keys.forEach(item => item.addEventListener('click', () => {
+
+  let target = item.dataset.key;
+
+  // Same as above but targets the div on click as opposed to keydown
+  const audio = document.querySelector(`audio[data-key="${target}"]`);
+  const key = document.querySelector(`.key[data-key="${target}"]`);
+
+  if ( !audio ) {
+    return;
+  }
+
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add('playing');
+
+}));
